7dba89249f97c6adac6cc9435a96d50406fdc766
abdoolmhh <57946234+abdoolmhh@users.noreply.github.com>
2023-09-06 07:31:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Set Streamlit page configuration
 st.set_page_config(
@@ -11,57 +8,37 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-# Title and Header
+# Title
 st.title("Network Traffic Analyzer")
-st.header("Analyze network traffic data effortlessly")
 
 # Sidebar - User Input
 st.sidebar.header("User Input")
 
 # Upload Data
-st.sidebar.subheader("Upload Network Traffic Data")
-uploaded_file = st.sidebar.file_uploader("Choose a PCAP file for analysis", type=["pcap", "pcapng"])
+uploaded_file = st.sidebar.file_uploader("Upload a PCAP file for analysis", type=["pcap", "pcapng"])
 
 # Options for Analysis
-st.sidebar.subheader("Select Analysis Options")
 show_summary = st.sidebar.checkbox("Show Traffic Summary")
 show_visualizations = st.sidebar.checkbox("Show Visualizations")
 
-# Function to Plot Pie Chart
-@st.cache(allow_output_mutation=True)
-def plot_pie_chart(data):
-    plt.figure(figsize=(8, 8))
-    sns.set_palette("Set3")
-    data['Packet_Category'].value_counts().plot(kind='pie', autopct='%1.1f%%', colors=sns.color_palette())
-    plt.title("Packet Category Distribution")
-
 # Main Content
-st.subheader("Network Traffic Analysis")
+st.sidebar.header("About")
+st.sidebar.write("This web app analyzes network traffic data and provides insights.")
+st.sidebar.write("Developed by [Your Name]")
 
 # Data Upload and Analysis
 if uploaded_file:
     st.success("File successfully uploaded. Analyzing...")
 
     # Perform analysis on uploaded_file (Use your analysis code here)
-    # Assuming you have a DataFrame named 'analysis_results' with columns like 'Packet_Category', 'Send_Receive', etc.
+    # Assuming you have a DataFrame or analysis results to display
 
     if show_summary:
         st.subheader("Traffic Summary")
         # Display summary data here (e.g., value counts of different packet categories)
-
-        if 'analysis_results' in locals():
-            packet_categories = analysis_results['Packet_Category'].value_counts()
-            st.write(packet_categories)
+        st.write("Summary data will be displayed here.")
 
     if show_visualizations:
         st.subheader("Traffic Visualizations")
-        # Display visualizations here (e.g., pie chart of packet categories)
-
-        if 'analysis_results' in locals():
-            plot_pie_chart(analysis_results)
-            st.pyplot()
-
-# About Section
-st.sidebar.header("About")
-st.sidebar.write("This web app is designed to analyze network traffic data and provide insights.")
-st.sidebar.write("Developed by [Your Name]")
+        # Display visualizations here (e.g., text-based visualizations)
+        st.write("Visualizations will be displayed here.")
