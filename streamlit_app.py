91349576b8c98a907c83bb74135883e62d9be423
abdoolmhh <57946234+abdoolmhh@users.noreply.github.com>
2023-09-25 00:10:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,36 +2,38 @@ import streamlit as st
 import pandas as pd
 import pyshark
 
-# Create a title and a sidebar
-st.title ("Packet Capture")
+# Create a title and a sidebar with a link
+st.title ("Packet Capture", anchor="title")
 st.sidebar.header ("Options")
 
-# Get the interface and filter from the user
-interface = st.sidebar.selectbox ("Select interface", ["1", "2", "3"])
-filter = st.sidebar.text_input ("Enter filter", value="http")
+# Get the interface and filter from the user with keys
+interface = st.sidebar.selectbox ("Select interface", ["1", "2", "3"], key="interface")
+filter = st.sidebar.text_input ("Enter filter", value="http", key="filter")
 
-# Create a button to start capturing
-if st.sidebar.button ("Start"):
-    # Capture packets from the selected interface and filter
-    capture = pyshark.LiveCapture (interface=interface, display_filter=filter)
-    # Create an empty dataframe to store the packet data
-    df = pd.DataFrame (columns=["Source IP", "Destination IP", "HTTP Method", "HTTP Host", "HTTP URI"])
-    # Loop through captured packets and append them to the dataframe
-    for packet in capture.sniff_continuously ():
-        df = df.append ({
+# Create a button to start capturing and get its value
+start = st.sidebar.button ("Start")
+
+# Capture packets from the selected interface and filter only if the button is clicked
+if start:
+    # Capture packets from the selected interface and filter with summaries
+    # Specify the tshark path as an argument
+    capture = pyshark.LiveCapture (interface=interface, display_filter=filter, tshark_path='/path/to/tshark', only_summaries=True)
+    
+    # Define a function that takes a packet and returns a dictionary of data
+    def get_packet_data (packet):
+        return {
             "Source IP": packet.ip.src,
             "Destination IP": packet.ip.dst,
             "HTTP Method": packet.http.request_method,
             "HTTP Host": packet.http.host,
             "HTTP URI": packet.http.request_uri
-        }, ignore_index=True)
-        # Display the dataframe as a table
-        st.table (df)
-        # Display a chart of packets by source IP address
-        st.bar_chart (df["Source IP"].value_counts ())
-        # Create a button to stop capturing
-        if st.button ("Stop"):
-            # Terminate the script execution
-            st.stop ()
-    # Create a button to download the captured packets as a CSV file
-    st.download_button ("Download CSV", df.to_csv (), file_name="packets.csv")
+        }
+
+    # Apply the function on each packet and get a list of results
+    packet_data = capture.apply_on_packets (get_packet_data)
+
+    # Create a dataframe from the list of results
+    df = pd.DataFrame (packet_data)
+
+    # Display the dataframe as a table with a wider width
+    st.table (df, width=800)
