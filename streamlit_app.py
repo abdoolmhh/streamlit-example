2b1bd922a23a43d085cec6929c3d9fd31edbafac
abdoolmhh <57946234+abdoolmhh@users.noreply.github.com>
2023-10-02 14:10:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,92 @@
+# Import libraries
 import streamlit as st
 import pandas as pd
-import pyshark
+import networkx as nx
+import matplotlib.pyplot as plt
 
-# Create a title and a sidebar with a link
-st.title ("Packet Capture", anchor="title")
-st.sidebar.header ("Options")
+# Define a function to optimize the network based on some criteria
+@st.cache # Cache the results of this function
+def optimize_network(network, criterion):
+    # This is a dummy function that returns the same network as input
+    # You can modify this function to implement your own optimization logic based on different criteria
+    return network
 
-# Get the interface and filter from the user with keys
-interface = st.sidebar.selectbox ("Select interface", ["1", "2", "3"], key="interface")
-filter = st.sidebar.text_input ("Enter filter", value="http", key="filter")
+# Define a function to generate feedback based on the network properties and criterion
+@st.cache # Cache the results of this function
+def generate_feedback(network, criterion):
+    # This is a dummy function that returns a generic feedback based on different criteria
+    # You can modify this function to provide more specific and helpful feedback based on different criteria
+    feedback = f"This is a feedback based on the network analysis and optimization using {criterion}."
+    return feedback
 
-# Create a button to start capturing and get its value
-start = st.sidebar.button ("Start")
+# Create a title for the app
+st.title("Network Analysis and Optimization App")
 
-# Capture packets from the selected interface and filter only if the button is clicked
-if start:
-    # Capture packets from the selected interface and filter with summaries
-    # Specify the tshark path as an argument
-    capture = pyshark.LiveCapture (interface=interface, display_filter=filter, tshark_path='/path/to/tshark', only_summaries=True)
-    
-    # Define a function that takes a packet and returns a dictionary of data
-    def get_packet_data (packet):
-        return {
-            "Source IP": packet.ip.src,
-            "Destination IP": packet.ip.dst,
-            "HTTP Method": packet.http.request_method,
-            "HTTP Host": packet.http.host,
-            "HTTP URI": packet.http.request_uri
-        }
+# Create a sidebar for user options and controls
+st.sidebar.header("User Options")
+# Create a selectbox widget to let the user choose an optimization criterion from a list of options
+criterion = st.sidebar.selectbox("Choose an optimization criterion", ["Degree Centrality", "Betweenness Centrality", "Closeness Centrality", "PageRank"])
+# Create a checkbox widget to let the user choose whether to show the network statistics or not
+show_stats = st.sidebar.checkbox("Show network statistics")
 
-    # Apply the function on each packet and get a list of results
-    packet_data = capture.apply_on_packets (get_packet_data)
+# Create a file uploader widget to upload the csv file
+uploaded_file = st.file_uploader("Upload a csv file", type="csv")
 
-    # Create a dataframe from the list of results
-    df = pd.DataFrame (packet_data)
+# If a file is uploaded, read it as a pandas dataframe
+if uploaded_file is not None:
+    # Show a progress bar while reading the csv file
+    with st.spinner("Reading csv file..."):
+        df = pd.read_csv(uploaded_file)
 
-    # Display the dataframe as a table with a wider width
-    st.table (df, width=800)
+    # Create a network from the dataframe using networkx library
+    # Assume that the dataframe has two columns: source and target
+    network = nx.from_pandas_edgelist(df, source="source", target="target")
+
+    # Optimize the network using the optimize_network function and the chosen criterion
+    optimized_network = optimize_network(network, criterion)
+
+    # Generate feedback using the generate_feedback function and the chosen criterion
+    feedback = generate_feedback(optimized_network, criterion)
+
+    # Visualize the network using matplotlib library and the chosen layout
+    plt.figure(figsize=(10, 10))
+    # Create a dictionary of possible layouts for the network visualization
+    layouts = {"Degree Centrality": nx.kamada_kawai_layout, "Betweenness Centrality": nx.spring_layout, "Closeness Centrality": nx.circular_layout, "PageRank": nx.random_layout}
+    # Choose the layout based on the criterion
+    layout = layouts[criterion]
+    # Draw the network with labels, colors and edges
+    nx.draw(optimized_network, pos=layout(optimized_network), with_labels=True, node_color="lightblue", edge_color="gray")
+    plt.title("Network Visualization")
+    plt.show()
+
+    # Display the network visualization using streamlit.pyplot function
+    st.pyplot()
+
+    # Display the feedback using streamlit.write function
+    st.write(feedback)
+
+    # If the user chooses to show the network statistics, display them using streamlit.write function
+    if show_stats:
+        st.write("Network Statistics")
+        st.write(f"Number of nodes: {network.number_of_nodes()}")
+        st.write(f"Number of edges: {network.number_of_edges()}")
+        st.write(f"Density: {nx.density(network)}")
+        st.write(f"Diameter: {nx.diameter(network)}")
+        st.write(f"Average clustering coefficient: {nx.average_clustering(network)}")
+
+# Create an expander section for additional information and details
+with st.beta_expander("More information"):
+    st.markdown("""
+    This app is designed to help you analyze and optimize your network data using different criteria. 
+    You can upload a csv file that contains two columns: source and target, representing the nodes and edges of your network. 
+    You can choose an optimization criterion from the sidebar, such as degree centrality, betweenness centrality, closeness centrality, or PageRank. 
+    The app will optimize your network based on the chosen criterion and display a visualization of the optimized network. 
+    The app will also generate a feedback based on the network analysis and optimization. 
+    You can also choose to show some basic network statistics, such as number of nodes, number of edges, density, diameter, and average clustering coefficient. 
+    For more information about network analysis and optimization, you can refer to these sources:
+
+- NetworkX Documentation
+- Streamlit Documentation
+- Network Analysis Made Simple
+- Network Optimization
+""")
